5e1196440fc24075187b27860abec3a6eb20bc08
AustinBaggio <austinbaggio@gmail.com>
2018-01-02 17:04:55 -0500

    Final

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shopify Front-End Dev Challenge 
+
+## Brief Overview 
+The css and js folder holds a local copy of the Bootstrap style sheets and linking files. 
+The index.html file holds the HTML, and it follows the Bootstrap grid structure. 
+The script.js file holds the validation code that is used to check the user inputs. 
+
+A local build would work by simply opening the index.html file in a browser
+
+# Author
+* **Austin Baggio** - [AustinBaggio](https://github.com/AustinBaggio)
---
+++ script.js
@@ -0,0 +1,24 @@
+/*function to validate email addresses and send the form data to the console.log 
+
+Approach was to use simple javascript and edit the DOM, the validation technique is a common addapted from StackOverflow
+https://stackoverflow.com/questions/15017052/understanding-email-validation-using-javascript
+
+*/
+
+function Validate() {
+    var mail = document.getElementById("email").value;
+    var interest = document.getElementById("interest").value;
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        //check if instance was left blank
+        if (interest == "") {
+            document.getElementById("error-email").innerHTML = 'please select an interest';
+        }
+        //log to the console if there is a valid email and interest 
+        console.log("Email: ", mail, " Interest: ", interest)
+        return (true)
+    }
+    document.getElementById("error-email").innerHTML = 'please enter a valid email';
+}
+
+
